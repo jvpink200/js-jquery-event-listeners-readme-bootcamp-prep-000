dc82d346209b33d4195f9020996b884f02cb30da
jvpink200 <jvillarr20@gmail.com>
2018-08-22 02:50:50 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,12 @@
 //define functions here
 
+
 $(document).ready(function(){
-
+  function getIt(){
+    $("p").on("click", function(event)){
+      alert("Hey!");
+    });
+  }
 // call functions here
-
+return getIt();
 });
